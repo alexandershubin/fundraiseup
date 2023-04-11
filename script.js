e0a3e22b53843e447e823495719b2d800fdc88dd
«Aleksandr» <ashubin@neoflex.ru>
2023-04-11 13:11:05 +0700

    main - refactor

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const lettersHtml = document.getElementById('letters');
-const answerHtml = document.getElementById('answer');
-const totalQuestionsHtml = document.getElementById('total_questions');
-const currentQuestionHtml = document.getElementById('current_question');
+const lettersElement = document.getElementById('letters');
+const answerElement = document.getElementById('answer');
+const totalQuestionsElement = document.getElementById('total_questions');
+const currentQuestionElement = document.getElementById('current_question');
 
 let count = 0;
 let errorCount = 0;
+const MAX_ERRORS = 3;
 
 let successObject = {
     word: 0,
@@ -13,7 +14,7 @@ let successObject = {
 
 const resultArray = [];
 
-const dictionary = [
+const words = [
     "apple",
     "function",
     "timeout",
@@ -31,7 +32,7 @@ const shuffleArray = (array) => {
     return array.sort(() => Math.random() - 0.5);
 }
 
-const randomTenArray = shuffleArray(dictionary).slice(0, 7);
+const randomWords = shuffleArray(words).slice(0, 7);
 
 const setLetter = (array, className) => {
     return array.map((item, index) => {
@@ -59,95 +60,11 @@ const getResultMaxErrorWord = (array) => {
         }
     }
 
-    return maxCountError ? randomTenArray[index] : '';
+    return maxCountError ? randomWords[index] : '';
 }
 
-const toNodeList = (arrayOfNodes) => {
-    const fragment = document.createDocumentFragment();
-    arrayOfNodes.forEach(function (item) {
-        fragment.appendChild(item.cloneNode(true));
-    });
-    return fragment.childNodes;
-};
-
-const render = () => {
-    const letterArray = randomTenArray[count];
-    let answerArray = [];
-    const resultError = [...resultArray].reduce((a, b) => a + b.error, 0);
-
-    answerHtml.innerHTML = '';
-    currentQuestionHtml.innerHTML = `${count + 1}`
-    totalQuestionsHtml.innerHTML = `${randomTenArray.length - 1}`;
-
-    const randomLetterArray = shuffleArray([...letterArray]);
-    lettersHtml.innerHTML = setLetter(randomLetterArray, 'btn-primary');
-    const buttons = document.querySelectorAll('.btn');
-
-    const addBtn = (buttons) => buttons.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            const index = answerArray.length;
-            const diff = letterArray[index] === e.target.innerText;
-
-            if (diff) {
-                answerArray.push(e.target.innerText);
-                answerHtml.innerHTML = setLetter(answerArray, 'btn-success');
-                btn.remove()
-                buttons.forEach(b => b.classList.remove('btn-danger'));
-            } else {
-                btn.classList.add('btn-danger');
-                errorCount++;
-            }
-
-            if (letterArray.length === answerArray.length) {
-                successObject = {error: errorCount, word: count};
-                resultArray.push(successObject)
-                count++;
-                errorCount = 0;
-                render();
-            }
-
-            if (errorCount >= 3) {
-                lettersHtml.innerHTML = '';
-                lettersHtml.innerHTML = setLetter(letterArray.split(''), 'btn-danger');
-            }
-        })
-    });
-
-    const addKey = (event) => {
-        const index = answerArray.length;
-        const diff = letterArray[index] === event.key;
-
-        if (diff) {
-            answerArray.push(event.key);
-            answerHtml.innerHTML = setLetter(answerArray, 'btn-success');
-
-            buttons.forEach(b =>  b.textContent === event.key && b.remove());
-            buttons.forEach(b => b.classList.remove('btn-danger'));
-        } else {
-            buttons.forEach(b => b.textContent === event.key && b.classList.add('btn-danger'));
-            errorCount++;
-        }
-
-        if (letterArray.length === answerArray.length) {
-            successObject = {error: errorCount, word: count};
-            resultArray.push(successObject)
-            count++;
-            errorCount = 0;
-            document.removeEventListener('keydown', addKey);
-            render();
-        }
-
-        if (errorCount >= 3) {
-            lettersHtml.innerHTML = '';
-            lettersHtml.innerHTML = setLetter(letterArray.split(''), 'btn-danger');
-        }
-    }
-
-    addBtn(buttons);
-    document.addEventListener('keydown', addKey);
-
-    if (count === 6) {
-        document.body.innerHTML = `<div class="modal fade show" style="display: block;">
+const renderResultModal = (resultError, resultArray) => {
+    document.body.innerHTML = `<div class="modal fade show" style="display: block;">
                                       <div class="modal-dialog modal-dialog-centered">
                                         <div class="modal-content">
                                           <div class="modal-header">
@@ -162,11 +79,81 @@ const render = () => {
                                         </div>
                                       </div>
                                     </div>`
+
+}
+
+const showSolution = (e, letterArray, answerArray, cl) => {
+    if (letterArray.length === answerArray.length) {
+        successObject = {error: errorCount, word: count};
+        resultArray.push(successObject)
+        count++;
+        errorCount = 0;
+        e.type === 'keydown' && document.removeEventListener('keydown', cl);
+        render();
     }
+
+    if (errorCount >= MAX_ERRORS) {
+        lettersElement.innerHTML = '';
+        lettersElement.innerHTML = setLetter(letterArray, 'btn-danger');
+    }
+};
+
+const render = () => {
+    const letterArray = randomWords[count];
+    let answerArray = [];
+    const resultError = [...resultArray].reduce((a, b) => a + b.error, 0);
+
+    answerElement.innerHTML = '';
+    currentQuestionElement.innerHTML = `${count + 1}`
+    totalQuestionsElement.innerHTML = `${randomWords.length - 1}`;
+
+    const randomLetterArray = shuffleArray([...letterArray]);
+    lettersElement.innerHTML = setLetter(randomLetterArray, 'btn-primary');
+    const buttons = document.querySelectorAll('.btn');
+
+    const handleClick = (e) => {
+        const index = answerArray.length;
+        const diff = letterArray[index] === e.target.innerText;
+
+        if (diff) {
+            answerArray.push(e.target.innerText);
+            answerElement.innerHTML = setLetter(answerArray, 'btn-success');
+            e.target.remove();
+            buttons.forEach(b => b.classList.remove('btn-danger'));
+        } else {
+            e.target.classList.add('btn-danger');
+            errorCount++;
+        }
+
+        showSolution(e, letterArray, answerArray)
+    };
+
+    const handleKeyDown = (e) => {
+        const index = answerArray.length;
+        const diff = letterArray[index] === e.key;
+
+        if (diff) {
+            answerArray.push(e.key);
+            answerElement.innerHTML = setLetter(answerArray, 'btn-success');
+
+            buttons.forEach(b => b.textContent === e.key && b.remove());
+            buttons.forEach(b => b.classList.remove('btn-danger'));
+        } else {
+            buttons.forEach(b => b.textContent === e.key && b.classList.add('btn-danger'));
+            errorCount++;
+        }
+
+        showSolution(e, letterArray, answerArray, handleKeyDown)
+    }
+    const addHandleClick = () => {
+        buttons.forEach(btn => {
+            btn.addEventListener('click', handleClick);
+        });
+    };
+
+    addHandleClick()
+    document.addEventListener('keydown', handleKeyDown);
+    count === 2 && renderResultModal(resultError, resultArray);
 }
 
 render();
-
-
-
-
